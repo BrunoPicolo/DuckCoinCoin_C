1b949ec40bcf986992b9f2159e440dd70d63a3f4
BrunoPicolo <38081700+BrunoPicolo@users.noreply.github.com>
2018-04-14 19:08:43 +0200

    Create Transaction.h

---
+++ Transaction.h
@@ -0,0 +1,62 @@
+#ifndef __TRANSACTION_H__
+#define __TRANSACTION_H__
+
+#define MAX_VALUE 10 
+/**
+ * @Transaction structure 
+ * 
+ */
+typedef struct s_transaction *Transaction ;
+typedef struct s_transactionDeque *TransactionDeque ;
+/**
+ * @ 
+ */
+void create_transaction() ;
+/**
+ * @brief [brief description]
+ * @details [long description]
+ * 
+ * @return [description]
+ */
+ TransactionDeque init_transaction_deque() ;
+/**
+ * @brief [brief description]
+ * @details [long description]
+ * 
+ * @param transaction [description]
+ * @return [description]
+ */
+void set_hash_transaction(Transaction  transaction) ;
+/**
+ * @brief [brief description]
+ * @details [long description]
+ * 
+ * @param t [description]
+ */
+void add_transaction_to_transactionDeque(TransactionDeque t) ;
+/**
+ * @brief [brief description]
+ * @details [long description]
+ * 
+ * @param transaction [description]
+ */
+void remove_transaction(TransactionDeque transaction, int index) ;
+/**
+ * @brief [brief description]
+ * @details [long description]
+ * 
+ * @param t [description]
+ */
+void delete_transaction_deque(TransactionDeque t) ;
+/**
+ * @brief [brief description]
+ * @details [long description]
+ * 
+ * @param transaction [description]
+ * @return [description]
+ */
+int get_index(Transaction transaction) ;
+
+#endif
+
+
